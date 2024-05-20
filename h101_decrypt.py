e36612f764521cc7372478918412050800c31954
moh4wk <cristiano.desimone@gmail.com>
2024-05-21 01:05:47 +0200

    Both encryptor and decryptor working. Data passed still hardcoded.

--- h101_decrypt.py
+++ h101_decrypt.py
@@ -1,52 +1,68 @@
 #!/usr/bin/env python3
-# test.py
+# h101_decrypt.py
 
 import binascii
 
 from Crypto.Cipher import AES
-from Crypto.Util.Padding import unpad, pad
-from Crypto.Random import get_random_bytes
+from Crypto.Util.Padding import unpad
 from rich.console import Console
 
 
 class Aes128Encryptor:
-    def __init__(self, encryption_key: str, data: str):
+    """
+    Decrypts an AES 128-bit encrypted string.
+    """
+
+    def __init__(self, encryption_key: str, data: bytes):
+        """
+        The encryption key needs to be the exact 16-char long string
+        used originally to encrypt the data.
+        :param encryption_key: Original 16-byte passphrase used for encryption.
+        :param data: The string of data which needs to be decrypted.
+        """
         self.c = Console()
-        self.iv = b""
         self.encryption_key = encryption_key.encode("utf-8")
-        self.data_to_encrypt = data.encode("utf-8")
-        self.padded_data = b""
-        self.encrypted_data = b""
-        self.base64_encoded_data = b""
+        self.data_to_decrypt = data
+        self.base64_decoded_data = b""
+        self.iv = b""
+        self.iv_stripped_data = b""
+        self.decrypted_data = b""
+        self.unpadded_data = b""
 
-    def generate_iv(self):
-        self.iv = get_random_bytes(16)
+    def base64_decode_data(self):
+        self.base64_decoded_data = binascii.a2b_base64(self.data_to_decrypt)
 
-    def pad_data(self):
-        self.padded_data = pad(self.data_to_encrypt, 16)
+    def extract_iv(self):
+        self.iv = self.base64_decoded_data[:16]
 
-    def aes128_encrypt_data(self):
+    def iv_strip_data(self):
+        self.iv_stripped_data = self.base64_decoded_data[16:]
+
+    def aes128_decrypt_data(self):
         cipher = AES.new(self.encryption_key, AES.MODE_CBC, self.iv)
-        self.encrypted_data = cipher.encrypt(self.padded_data)
+        self.decrypted_data = cipher.decrypt(self.iv_stripped_data)
 
-    def base64_encode_data(self):
-        self.base64_encoded_data = binascii.b2a_base64(self.encrypted_data).strip()
+    def unpad_data(self):
+        self.unpadded_data = unpad(self.decrypted_data, 16)
 
-    def encrypt(self):
-        self.generate_iv()
-        self.c.print(f"[+] IV: {self.iv}", style="bright_red")
-        self.pad_data()
-        self.c.print(f"[+] Padded data: {self.padded_data}", style="green_yellow")
-        self.aes128_encrypt_data()
-        self.c.print(f"[+] AES 128 Encrypted data: {self.encrypted_data}", style="dark_cyan")
-        self.base64_encode_data()
-        self.c.print(f"[+] Base64 encoded AES 128 encrypted data: {self.base64_encoded_data}", style="green4")
+    def decrypt(self):
+        self.base64_decode_data()
+        self.c.print(f"[+] Base64 decoded AES 128 encrypted data: {self.base64_decoded_data}", style="bright_red")
+        self.extract_iv()
+        self.c.print(f"[+] IV: {self.iv}", style="green_yellow")
+        self.iv_strip_data()
+        self.c.print(f"[+] IV-stripped base64 decoded AES 128 encrypted data: {self.iv_stripped_data}",
+                     style="cyan3")
+        self.aes128_decrypt_data()
+        self.c.print(f"[+] AES 128 decrypted data: {self.decrypted_data}", style="dark_cyan")
+        self.unpad_data()
+        self.c.print(f"[+] Unpadded data: {self.unpadded_data}", style="green4")
 
 
 def main():
-    data_to_encrypt = "Hello snake!"
-    encryptor = Aes128Encryptor("sixteenbytekey!!", data_to_encrypt)
-    encryptor.encrypt()
+    data_to_decrypt = b'eCuu+sQfXBkvPHwChvtL2KRzwc4h+7SodYFOmxrRzxw='
+    encryptor = Aes128Encryptor("sixteenbytekey!!", data_to_decrypt)
+    encryptor.decrypt()
 
 
 if __name__ == "__main__":
