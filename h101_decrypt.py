05160a16f2c44bc44e441e1b28483de4e968e42b
moh4wk <cristiano.desimone@gmail.com>
2024-05-20 23:00:48 +0200

    Created decryptor h101_decrypt.py.

--- h101_decrypt.py
+++ h101_decrypt.py
@@ -1,39 +1,53 @@
 #!/usr/bin/env python3
-# h101_decrypt.py
+# test.py
+
 import binascii
 
 from Crypto.Cipher import AES
-from Crypto.Util.Padding import unpad
-
-data = ("A4nEuSRBiqMVqAzfKPap2n0CJ8eL5J8D3uymb-diUhMzGqRqQ4HiRd-"
-        "vZKF5665hJU7Gfy08WeNyCZ8ViA2M0w4eC8jkFRRvHs1lpuKs2hcXJ!"
-        "cIn4Ety2tBjQjggQCFS5BVF9154P0fpfdPdCmn7IBsp!YGVpii5bjXJ"
-        "CpS9uygVcY48JqoSnm-CnzbTB624!vhXkSEcjPGWH9NVcpNBQ~~")
+from Crypto.Util.Padding import unpad, pad
+from Crypto.Random import get_random_bytes
+from rich.console import Console
 
 
-def correct_data(incorrect_data: str):
-    return incorrect_data.replace("~", "=").replace("-", "+").replace("!", "/")
+class Aes128Encryptor:
+    def __init__(self, encryption_key: str, data: str):
+        self.c = Console()
+        self.iv = b""
+        self.encryption_key = encryption_key.encode("utf-8")
+        self.data_to_encrypt = data.encode("utf-8")
+        self.padded_data = b""
+        self.encrypted_data = b""
+        self.base64_encoded_data = b""
+
+    def generate_iv(self):
+        self.iv = get_random_bytes(16)
+
+    def pad_data(self):
+        self.padded_data = pad(self.data_to_encrypt, 16)
+
+    def aes128_encrypt_data(self):
+        cipher = AES.new(self.encryption_key, AES.MODE_CBC, self.iv)
+        self.encrypted_data = cipher.encrypt(self.padded_data)
+
+    def base64_encode_data(self):
+        self.base64_encoded_data = binascii.b2a_base64(self.encrypted_data).strip()
+
+    def encrypt(self):
+        self.generate_iv()
+        self.c.print(f"[+] IV: {self.iv}", style="bright_red")
+        self.pad_data()
+        self.c.print(f"[+] Padded data: {self.padded_data}", style="green_yellow")
+        self.aes128_encrypt_data()
+        self.c.print(f"[+] AES 128 Encrypted data: {self.encrypted_data}", style="dark_cyan")
+        self.base64_encode_data()
+        self.c.print(f"[+] Base64 encoded AES 128 encrypted data: {self.base64_encoded_data}", style="green4")
 
 
-corrected_data = correct_data(data)
-print(f"Corrected data: {corrected_data}")
+def main():
+    data_to_encrypt = "Hello snake!"
+    encryptor = Aes128Encryptor("sixteenbytekey!!", data_to_encrypt)
+    encryptor.encrypt()
 
-decoded_data = binascii.a2b_base64(corrected_data)
-print(f"Decoded data: {decoded_data}")
 
-# Extract IV (first 16 chars of string)
-
-iv = decoded_data[:16]
-cipher_text = decoded_data[16:]
-
-print(f"IV: {iv}")
-print(f"Cipher text: {cipher_text}")
-
-# Placeholder for the key (actual length of placeholder 16 bytes)
-key = b"sexteenbytekey!!"
-
-# Create AES cypher and decrypt the cypher text
-cipher = AES.new(key, AES.MODE_CBC, iv)
-decrypted_data = unpad(cipher.decrypt(cipher_text), AES.block_size)
-
-print(f"Decrypted data: {decrypted_data.decode('utf-8')}")
+if __name__ == "__main__":
+    main()
